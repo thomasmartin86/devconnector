3babac66312fb1c9a429b0d4263676c25fcb39c8
thomasmartin86 <thomas.martin1126@gmail.com>
2018-09-11 16:43:26 -0400

    Corrected production keys issue preventing heroku deployment..keys issues

--- config/passport.js
+++ config/passport.js
@@ -1,9 +1,8 @@
-//define strategy
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
-const keys = require('./keys');
+const keys = require('../config/keys');
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
@@ -12,12 +11,12 @@ opts.secretOrKey = keys.secretOrKey;
 module.exports = passport => {
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
-      console.log(jwt_payload);
-
       User.findById(jwt_payload.id)
         .then(user => {
-          if (user) return done(null, user);
-          else return done(null, false);
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
         })
         .catch(err => console.log(err));
     })
--- server.js
+++ server.js
@@ -1,42 +1,59 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
+//bring in express, mongoose, body-parser and passport
+const express = require('express'); //express
+const mongoose = require('mongoose'); //mongoose (interact with mongodb connection)
+const bodyParser = require('body-parser'); //express middleware
+const passport = require('passport'); //use for authentication
 const path = require('path');
-
+//define routes
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
+//create new instance of express assigned to app const
 const app = express();
 
-// Body parser middleware
+/*
+   body.parser middleware
+   body-parser extracts the entire body portion of an incoming request stream and exposes it on req.body.
+   
+   -bodyParser.urlencoded({extended: ...}) 
+     basically tells the system whether you want to use a simple algorithm for shallow parsing (i.e. false) 
+     or complex algorithm for deep parsing that can deal with nested objects (i.e. true).
+
+   -bodyParser.json() 
+     tells the system that you want json to be used.
+*/
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// DB Config
+//db config using exported keys
 const db = require('./config/keys').mongoURI;
 
-// Connect to MongoDB
+//connect to mongo using mongoose
+//returns a promise
 mongoose
   .connect(db)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-// Passport middleware
+//create a simple route to homepoge and return
+app.get('/', (req, res) => res.send('Hello World!'));
+
+//Passport middleware
 app.use(passport.initialize());
 
-// Passport Config
+//passport config
 require('./config/passport')(passport);
 
-// Use Routes
+//use routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-// Server static assets if in production
+//serve static assests if in production
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
+  //set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
@@ -44,6 +61,8 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+//process env port for heroku deployment, locally run on 500
 const port = process.env.PORT || 5000;
 
+//listen on port defined above, and console it out
 app.listen(port, () => console.log(`Server running on port ${port}`));
