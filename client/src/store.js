fc948d7a984c4c85c6e8fdcef834831516867bff
thomasmartin86 <thomas.martin1126@gmail.com>
2018-09-21 13:21:53 -0400

    fixing dev tools

--- client/src/store.js
+++ client/src/store.js
@@ -7,12 +7,11 @@ import rootReducer from './reducers';
 const initialState = {};
 const middleware = [thunk];
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 //create store
 const store = createStore(
   rootReducer,
   initialState,
-  composeEnhancers(applyMiddleware(...middleware))
+  compose(applyMiddleware(...middleware))
 );
+
 export default store;
