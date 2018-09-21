a184c8f56063f4f16b8fac1b9ccce2d0f7230e3c
thomasmartin86 <thomas.martin1126@gmail.com>
2018-09-21 14:00:43 -0400

    fix

--- client/src/store.js
+++ client/src/store.js
@@ -7,18 +7,11 @@ import rootReducer from './reducers';
 const initialState = {};
 const middleware = [thunk];
 
-const whichCompose = () => {
-  if (window.navigator.userAgent.includes('Chrome')) {
-    compose(
-      applyMiddleware(...middleware),
-      // Implements the Chrome redux tools extension
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
-  } else {
-    compose(applyMiddleware(...middleware));
-  }
-};
+//create store
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(applyMiddleware(...middleware))
+);
 
-const store = createStore(rootReducer, initialState, whichCompose());
 export default store;
